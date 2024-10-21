a6345eb0ed56c5e7e24a2237bba6935bb004f73b
SamFernFer <>
2024-10-21 06:17:56 -0300

    There are still some dependencies on the standard library's DLLs, but they are of no relevance.

--- src/Program.cpp
+++ src/Program.cpp
@@ -15,24 +15,24 @@ namespace Fenton::Minrzbas {
     constexpr const char* versionString = "1.0.0";
 
     int main(int argc, char** argv) {
-        // po::option_description _desc("Allowed options");
-        // _desc.add_options()
-        //     ("help,h", "Displays help information.")
-        //     ("version,V", "Displays the program's version.")
-        // ;
-        // po::variables_map _vm;
-        // po::store(po::parse_command_line(argc, argv, _desc), _vm);
-        // po::notify(_vm);
+        po::options_description _desc("Allowed options");
+        _desc.add_options()
+            ("help,h", "Displays help information.")
+            ("version,V", "Displays the program's version.")
+        ;
+        po::variables_map _vm;
+        po::store(po::parse_command_line(argc, argv, _desc), _vm);
+        po::notify(_vm);
 
-        // if (_vm.count("help")) {
-        //     std::cout << _desc << "\n";
-        //     return 1;
-        // }
-        // if (_vm.count("version")) {
-        //     std::cout << versionString << "\n";
-        //     return 1;
-        // }
-        std::cout << boost::json::array({0, 1, 9}) << std::endl;
+        if (_vm.count("help")) {
+            std::cout << _desc << "\n";
+            return 1;
+        }
+        if (_vm.count("version")) {
+            std::cout << versionString << "\n";
+            return 1;
+        }
+        // std::cout << boost::json::array({0, 1, 9}) << std::endl;
         return 0;
     }
 }
